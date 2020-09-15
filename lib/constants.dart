501758dc4b1868b98f9b177b92905e2b72162ce8
Lennard <lennarddeurman@live.nl>
2020-09-15 14:31:45 +0200

    Created action items

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,16 +3,16 @@ import 'package:flutter/material.dart';
 
 class Constants {
 
-  static String appNameWithUrl = "Ikleeralles.nl";
-  static String appName = "Ikleeralles";
+  static const String appNameWithUrl = "Ikleeralles.nl";
+  static const String appName = "Ikleeralles";
 
 }
 
 class Fonts {
 
-  static String justAnotherHand = "JustAnotherHand";
-  static String montserrat = "Montserrat";
-  static String ubuntu = "Ubuntu";
+  static const String justAnotherHand = "JustAnotherHand";
+  static const String montserrat = "Montserrat";
+  static const String ubuntu = "Ubuntu";
 
 }
 
@@ -30,28 +30,42 @@ class BrandColors {
   static Color inputErrorColor = Color.fromRGBO(210, 100, 100, 1);
   static Color labelColorYellow = Color.fromRGBO(251, 220, 20, 1);
   static Color textColorLighter = Color.fromRGBO(104, 104, 104, 1);
+  static Color badgeTextColor = Color.fromRGBO(138, 135, 135, 1);
+  static Color badgeBackgroundColor = Color.fromRGBO(232, 232, 232, 1);
 }
 
 class TranslationKeys {
 
-  static String usernameOrEmail = "usernameOrEmail";
-  static String usernameHint = "usernameHint";
-  static String password = "password";
-  static String forgotPassword = "forgotPassword";
-  static String signIn = "signIn";
-  static String createAnAccount = "createAnAccount";
-  static String emptyTextError = "emptyTextError";
-  static String register = "register";
-  static String okay = "okay";
-  static String emailSent = "emailSent";
-  static String recoverEmailSentMessage = "recoverEmailSentMessage";
-  static String loginError = "loginError";
-  static String registrationError = "registrationError";
-  static String loadingInProgress = "loadingInProgress";
+  static const String usernameOrEmail = "usernameOrEmail";
+  static const String usernameHint = "usernameHint";
+  static const String password = "password";
+  static const String forgotPassword = "forgotPassword";
+  static const String signIn = "signIn";
+  static const String createAnAccount = "createAnAccount";
+  static const String emptyTextError = "emptyTextError";
+  static const String register = "register";
+  static const String okay = "okay";
+  static const String emailSent = "emailSent";
+  static const String recoverEmailSentMessage = "recoverEmailSentMessage";
+  static const String loginError = "loginError";
+  static const String registrationError = "registrationError";
+  static const String loadingInProgress = "loadingInProgress";
+  static const String itemsBadge = "itemsBadge";
+  static const String myLists = "myLists";
+  static const String trashCan = "trashCan";
+  static const String myFolders = "myFolders";
+  static const String publicLists = "publicLists";
+  static const String publicListsDescription = "publicListsDescription";
 
 }
 
 class WebUrls {
-  static String register = "https://www.ikleeralles.nl/aanmelden";
-  static String forgotPassword = "https://www.ikleeralles.nl/requestpassword";
+  static const String register = "https://www.ikleeralles.nl/aanmelden";
+  static const String forgotPassword = "https://www.ikleeralles.nl/requestpassword";
+}
+
+class AssetPaths {
+  static const String internet = 'assets/svg/internet.svg';
+  static const String rootDirectory = 'assets/svg/root_directory.svg';
+  static const String trash = 'assets/svg/trash.svg';
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,8 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_i18n/flutter_i18n.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:ikleeralles/constants.dart';
 import 'package:ikleeralles/ui/appbar.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,15 +14,220 @@ class HomePage extends StatefulWidget {
 
 }
 
+class InnerCell extends StatelessWidget {
+
+  final Widget leading;
+  final Widget trailing;
+  final String title;
+  final String subTitle;
+
+  InnerCell ({ @required this.leading, this.trailing, @required this.title, this.subTitle });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: <Widget>[
+          Container(
+            child: this.leading,
+            margin: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10
+            ),
+          ),
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                  vertical: 10
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      this.title,
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: Fonts.montserrat,
+                          fontWeight: FontWeight.w600
+                      ),
+                    ),
+                  ),
+                  Visibility(
+                    child: Container(
+                      margin: EdgeInsets.only(top: 4),
+                      child: Text(
+                        this.subTitle ?? "",
+                        style: TextStyle(
+                            fontSize: 12,
+                            fontFamily: Fonts.montserrat,
+                            fontWeight: FontWeight.w500
+                        ),
+                      ),
+                    ),
+                    visible: this.subTitle != null,
+                  )
+                ],
+              ),
+            ),
+          ),
+          Visibility(child: Container(
+            margin: EdgeInsets.symmetric(horizontal: 20),
+            child: this.trailing ?? Container(),
+          ), visible: this.trailing != null)
+        ],
+      ),
+    );
+  }
+
+}
+
+abstract class ActionCell extends StatelessWidget {
+
+  final Function onPressed;
+
+  ActionCell ({ this.onPressed });
+
+  Widget badge(BuildContext context, { @required int count  }) {
+    return Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: BrandColors.badgeBackgroundColor
+      ),
+      padding: EdgeInsets.symmetric(
+        vertical: 7,
+        horizontal: 15
+      ),
+      child: Text(
+        FlutterI18n.translate(context, TranslationKeys.itemsBadge, { "count": count.toString() }),
+        style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w500,
+            color: BrandColors.badgeTextColor
+        ),
+      ),
+    );
+  }
+
+  Widget create({ String iconAssetPath, String title, String subTitle, Widget trailing }) {
+    return Material(child: InkWell(
+      borderRadius: BorderRadius.all(Radius.circular(12)),
+      child: DottedBorder(
+          strokeWidth: 1.5,
+          borderType: BorderType.RRect,
+          radius: Radius.circular(12),
+          color: BrandColors.borderColor,
+          dashPattern: [6, 4],
+          child: ClipRRect(
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+            child: InnerCell(
+              leading: SvgPicture.asset(
+                  iconAssetPath
+              ),
+              title: title,
+              subTitle: subTitle,
+              trailing: trailing,
+            ),
+          )
+      ),
+      onTap: onPressed,
+    ), color: Colors.white);
+  }
+
+
+}
+
+class PublicListsActionCell extends ActionCell {
+
+  PublicListsActionCell ({ Function onPressed }) : super(onPressed: onPressed);
+
+  @override
+  Widget build(BuildContext context) {
+    return create(
+      iconAssetPath: AssetPaths.internet,
+      title: FlutterI18n.translate(context, TranslationKeys.publicLists),
+      subTitle: FlutterI18n.translate(context, TranslationKeys.publicListsDescription)
+    );
+  }
+
+}
+
+class MyFoldersActionCell extends ActionCell {
+
+  final int count;
+
+  MyFoldersActionCell ({ Function onPressed, this.count }) : super(onPressed: onPressed);
+
+  @override
+  Widget build(BuildContext context) {
+    return create(
+      iconAssetPath: AssetPaths.rootDirectory,
+      title: FlutterI18n.translate(context, TranslationKeys.myFolders),
+      trailing: badge(context, count: count)
+    );
+  }
+
+}
+
+class TrashActionCell extends ActionCell {
+
+  final int count;
+
+  TrashActionCell ({ Function onPressed, this.count }) : super(onPressed: onPressed);
+
+  @override
+  Widget build(BuildContext context) {
+    return create(
+        iconAssetPath: AssetPaths.trash,
+        title: FlutterI18n.translate(context, TranslationKeys.trashCan),
+        trailing: badge(context, count: count)
+    );
+  }
+
+}
+
 class HomePageState extends State<HomePage> {
+
+  void _onTrashPressed() {
+
+  }
+
+  void _onMyFoldersPressed() {
+
+  }
+
+  void _onPublicListsPressed() {
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: ThemedAppBar(
-        title: "Mijn lijsten",
+        title: FlutterI18n.translate(context, TranslationKeys.myLists),
       ),
-      body: Container(),
+      body: Container(
+        child: ListView(
+          padding: EdgeInsets.all(
+              15
+          ),
+          children: <Widget>[
+            TrashActionCell(
+              count: 11,
+              onPressed: _onTrashPressed,
+            ),
+            MyFoldersActionCell(
+              count: 20,
+              onPressed: _onMyFoldersPressed,
+            ),
+            PublicListsActionCell(
+              onPressed: _onPublicListsPressed,
+            )
+          ],
+        ),
+        color: Colors.white,
+      ),
 
     );
   }
