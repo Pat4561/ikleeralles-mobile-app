9797587132d6776ed3daa053147f6428b4e2e8ba
deurman <lennarddeurman@live.nl>
2020-11-26 13:01:42 +0100

    Removed all cupertino references

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:ikleeralles/constants.dart';
 import 'package:ikleeralles/logic/managers/operation.dart';
@@ -10,6 +9,7 @@ import 'package:ikleeralles/logic/operations/trash.dart';
 import 'package:ikleeralles/network/models/exercise_list.dart';
 import 'package:ikleeralles/pages/exercise_list.dart';
 import 'package:ikleeralles/pages/exercises_overview.dart';
+import 'package:ikleeralles/ui/navigation_drawer.dart';
 import 'package:ikleeralles/ui/tables/exercises_overview.dart';
 import 'package:ikleeralles/ui/tables/search.dart';
 import 'package:ikleeralles/ui/themed/appbar.dart';
@@ -91,62 +91,6 @@ class _HomePageDrawer extends StatelessWidget {
             onTap: () => onPressed(keyLogout),
           )
         ],
-      ),
-    );
-  }
-
-}
-
-
-abstract class NavigationDrawerContentChild {
-
-  final String key;
-  final NavigationDrawerController navigationDrawerController;
-
-  String get title;
-
-  Widget body(BuildContext context);
-
-  Widget bottomNavigationBar(BuildContext context) {
-    return null;
-  }
-
-  Widget floatingActionButton(BuildContext context) {
-    return null;
-  }
-
-  Widget appBar(BuildContext context, GlobalKey<ScaffoldState> scaffoldKey) {
-    return navigationDrawerController.appBar(context, scaffoldKey, title);
-  }
-   
-  NavigationDrawerContentChild (this.navigationDrawerController, { @required this.key });
-
-}
-
-class NavigationDrawerController {
-
-  ValueNotifier<NavigationDrawerContentChild> _notifier;
-
-  ValueNotifier<NavigationDrawerContentChild> get notifier => _notifier;
-
-  NavigationDrawerContentChild get activeChild => _notifier.value;
-
-  set activeChild (NavigationDrawerContentChild child) => _notifier.value = child;
-
-  NavigationDrawerController () {
-    _notifier = ValueNotifier<NavigationDrawerContentChild>(null);
-  }
-
-  Widget appBar(BuildContext context, GlobalKey<ScaffoldState> scaffoldKey, String title) {
-
-    return ThemedAppBar(
-      title: title,
-      disablePopping: true,
-      showUserInfo: true,
-      leading: IconButton(
-        padding: EdgeInsets.all(0),
-        icon: Icon(Icons.menu, color: Colors.white),
-        onPressed: () => scaffoldKey.currentState.openDrawer(),
       ),
     );
   }
@@ -245,10 +189,6 @@ class _MyExercisesSubPage extends NavigationDrawerContentChild {
   }
 
 }
-
-
-
-
 
 
 class _PublicSearchSubPage extends NavigationDrawerContentChild {
--- lib/pages/quiz.dart
+++ lib/pages/quiz.dart
@@ -1,5 +1,5 @@
 import 'dart:math';
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_i18n/flutter_i18n.dart';
 import 'package:flutter_svg/flutter_svg.dart';
